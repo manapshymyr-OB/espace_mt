fe1bf00535f5d47ebb45e3bb0a150536deeaf786
Manap Shymyr <manap.shymyr@tum.de>
2024-05-07 22:12:10 +0200

    test

--- pickle_merge.py
+++ pickle_merge.py
@@ -8,22 +8,39 @@ with open(pickle_chunk, "rb") as f:
     building_ids = pickle.load(f).keys()
 ndvi = []
 counter = 0
+ndvi_mean_lst = []
+ndvi_min_lst = []
+ndvi_max_lst = []
+b_ids = []
 for id in building_ids:
     try:
         ndvi_pickle_file = os.path.join(data_dir, str(id))
         with open(ndvi_pickle_file, 'rb') as f:
              ndvi_df = pickle.load(f)
 
-        ndvi.append(ndvi_df)
+        # ndvi.append(ndvi_df)
+        # print(ndvi_df)
+        ndvi_mean_lst.append(ndvi_df['ndvi'].mean())
+        ndvi_min_lst.append(ndvi_df['ndvi'].min())
+        ndvi_max_lst.append(ndvi_df['ndvi'].max())
+        b_ids.append(id)
+
         counter+=1
         print(counter)
     except Exception as e:
         print(e)
     # print(id)
 
-print(counter)
-df = pd.concat(ndvi)
 
+data = {
+
+    'building_ids': b_ids,
+    'ndvi_mean': ndvi_mean_lst,
+    'ndvi_min': ndvi_min_lst,
+    'ndvi_max': ndvi_max_lst
+}
+df = pd.DataFrame.from_dict(data)
+print(df)
 df.to_pickle(f'ndvi_{pickle_file}')
 
 # print(building_ids[0])
